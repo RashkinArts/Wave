365bfc046132ea90a8901dad026c48f819d16413
RashkinArts <109157878+RashkinArts@users.noreply.github.com>
2022-08-17 19:42:15 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,68 @@
+
+//more_podcasts
+
+const morePodcasts = document.querySelectorAll('.more_podcasts');
+const moreBtn = document.querySelector('#more_podcasts_btn');
+
+  moreBtn.addEventListener('click', function() {
+    morePodcasts.forEach(function(card) {
+      card.classList.add('is_active')
+    });
+    moreBtn.remove();
+  });
+
+//select
+
+const element = document.querySelector('#selectCustom')
+const choices = new Choices(element, {
+  searchEnabled: false,
+  shouldSort: false,
+  itemSelectText: '', 
+})
+
+
+//form
+
+
+new JustValidate('.js-form', {
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      maxLength: 12,
+    },
+  
+    mail: {
+      required: true,
+      email: true
+    },
+  
+  },
+  
+  messages: {
+    name: 'Ошибка',
+    mail: 'Ошибка',
+  },
+  
+  colorWrong: '#FF6F6F'
+  
+  })
+
+//accordion
+ $(function () {
+ $("#accordion").accordion();
+ });
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
